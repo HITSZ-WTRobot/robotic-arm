e66428679d0e94fa8ac169f9b9f2586bc38fee43
LIJunHong659 <1346469839@qq.com>
2025-11-23 20:33:40 +0800

    add Unitree_PID

--- UserCode/app/app.cpp
+++ UserCode/app/app.cpp
@@ -14,6 +14,35 @@ DJI_t dji;
 
 Motor_PosCtrl_t pos_dji;
 
+MotorPID_t Unitree_PID;
+
+
+
+void Unitree_PID_OutPut(MotorPID_t *Unitree_PID, UnitreeMotor* g_motor, float Unitree_target_pos)
+{
+    Unitree_PID->fdb = g_motor->data.Pos;   // pid位置反馈值为宇树电机位置返回值
+    MotorPID_Calculate(Unitree_PID);
+    Unitree_UART_tranANDrev(g_motor,
+                                ID,
+                                1,                     // mode
+                                Unitree_PID->output,   // tau
+                                0.0f,                  // W
+                                Unitree_target_pos,    // Pos
+                                0.0f,                  // K_P
+                                0.0f                   // K_W
+                            );
+                            
+}
+
+void Unitree_PID_Init(MotorPID_t *Unitree_PID)
+{
+    Unitree_PID->Kp                 = 0.0f;
+    Unitree_PID->Ki                 = 0.0f;
+    Unitree_PID->Kd                 = 0.0f;
+    Unitree_PID->abs_output_max     = 0.0f;
+}
+
+
 /**
  * 定时器回调函数，用于定时进行 PID 计算和 CAN 指令发送
  * @param htim
@@ -22,11 +51,13 @@ void TIM_Callback(TIM_HandleTypeDef* htim)
 {
     Motor_PosCtrlUpdate(&pos_dji); // 进行 PID 计算
 
-    
+
     // TransmitData();
     // 发送控制信号
     DJI_SendSetIqCommand(&hcan1, IQ_CMD_GROUP_1_4);
     // DJI_SendSetIqCommand(&hcan1, IQ_CMD_GROUP_5_8);
+
+    Unitree_PID_OutPut(Unitree_PID, g_motor, Unitree_target_pos);
 }
 
 void DJI_Control_Init()
@@ -73,20 +104,20 @@ void DJI_Control_Init()
 }
 
 float target_angle = 0.0f;
+float Unitree_target_pos = 0.0f;
+// // 使用时需要修改为对应的电机ID，串口句柄，kp，kd参数，与RE，DE引脚设置（在Unitree_user.h）
 
-// 使用时需要修改为对应的电机ID，串口句柄，kp，kd参数，与RE，DE引脚设置（在Unitree_user.h）
-
-// 前馈力矩计算
-// y = 0.002 * x^3 - 0.0496 * x^2 + 0.0611 * x + 1.8155
-// y = ((0.002 * x - 0.0496) * x + 0.0611) * x + 1.8155
-float tau_Cal(float angle)
-{
-    float tau = 0.002f;
-    tau       = tau * angle - 0.0496f;
-    tau       = tau * angle + 0.0611f;
-    tau       = tau * angle + 1.8155f;
-    return tau;
-}
+// // 前馈力矩计算
+// // y = 0.002 * x^3 - 0.0496 * x^2 + 0.0611 * x + 1.8155
+// // y = ((0.002 * x - 0.0496) * x + 0.0611) * x + 1.8155
+// float tau_Cal(float angle)
+// {
+//     float tau = 0.002f;
+//     tau       = tau * angle - 0.0496f;
+//     tau       = tau * angle + 0.0611f;
+//     tau       = tau * angle + 1.8155f;
+//     return tau;
+// }
 
 // 电机初始化任务
 
@@ -105,23 +136,28 @@ void Init(void* argument)
     }
 
     DJI_Control_Init(); // 初始化大疆电机
+    Unitree_PID_Init(&Unitree_PID);     // 初始化宇树电机pid参数
     osThreadExit();
 }
 
 void MotorCtrl(void* argument)
 {
+    // 设置宇树电机目标位置
+    Unitree_target_pos = 1.0f;
+
     for (;;)
     {
-        Unitree_UART_tranANDrev(g_motor,
-                                ID,
-                                1,                     // mode
-                                tau_Cal(target_angle), // tau
-                                1.0f,                  // W
-                                0.0f,                  // Pos
-                                0.0f,                  // K_P
-                                0.1f                   // K_W
-        );
-        osDelayUntil(10);
+        // Unitree_UART_tranANDrev(g_motor,
+        //                         ID,
+        //                         1,                     // mode
+        //                         tau_Cal(target_angle), // tau
+        //                         0.0f,                  // W
+        //                         0.0f,                  // Pos
+        //                         0.0f,                  // K_P
+        //                         0.1f                   // K_W
+        // );
+        // osDelayUntil(10);
+        osDelay(100);
     }
 }
 }
